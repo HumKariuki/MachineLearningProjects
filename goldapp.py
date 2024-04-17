d0b9771b537b11416e1816306c8cb110f497d947
Humphrey Kariuki <83478885+HumKariuki@users.noreply.github.com>
2024-04-17 10:22:43 +0300

    Update goldapp.py

--- goldapp.py
+++ goldapp.py
@@ -4,7 +4,7 @@ import pickle
 import streamlit as st
 
 # Loading the saved model
-loaded_model = pickle.load(open('goldmodel.sav', 'rb'))
+loaded_model = pickle.load(open('goldmodel.pkl', 'rb'))
 
 def gold_price_prediction(input_data):
     # Reshape the input data as we are predicting for only one instance
